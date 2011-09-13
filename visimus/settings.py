a2ca8957d31ad10d90d42bc2ebcf22f05b0fe264
Pedro Kroger <Pedro Kroger pedro.kroger@gmail.com>
2011-09-13 12:13:16 -0300

    Change settings to work on webfaction

--- visimus/settings.py
+++ visimus/settings.py
@@ -1,80 +1,91 @@
-# Django settings for visimus project.
-
-import sys
+import logging
 import os
+import os.path
+import socket
+import json
+import unicodedata
 
-SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+HOST = socket.gethostname()
+MAINDIR = os.path.dirname(os.path.abspath( __file__ ))
+LOGIN_URL = "/login/"
+STATIC_URL = '/static/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+
+if HOST == 'web187.webfaction.com':
+    filename = os.path.join(MAINDIR, "serverconf.json")
+    with open(filename) as conf_file:
+        serverconf = json.load(conf_file)
+
+    PRODUCTION = True
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'visimus',
+            'USER': 'visimus',
+            'PASSWORD': serverconf['db-password'],
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+            }
+        }
+
+    SEND_BROKEN_LINK_EMAILS = False
+    EMAIL_HOST = 'smtp.webfaction.com'
+    EMAIL_HOST_USER = 'kroger'
+    password = serverconf['email-password']
+    EMAIL_HOST_PASSWORD = unicodedata.normalize('NFKD', password).encode('ascii','ignore')
+    DEFAULT_FROM_EMAIL = 'kroger@pedrokroger.net'
+    SERVER_EMAIL = DEFAULT_FROM_EMAIL
+
+    LOGFILE = os.path.join(MAINDIR, 'log', 'visimus.log')
+    STATIC_ROOT = '/home/kroger/webapps/visimus_static/'
+    MEDIA_ROOT = ''
+
+else:
+    PRODUCTION = False
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(MAINDIR, 'data.db'),
+            }
+        }
+
+    LOGFILE = os.path.join(MAINDIR, 'log', 'visimus.log')
+    STATIC_ROOT = os.path.join(MAINDIR, "testapp/static/")
+
+
+if PRODUCTION:
+    DEBUG = False
+    TEMPLATE_DEBUG = DEBUG
+    LOG_LEVEL = logging.DEBUG
+else:
+    DEBUG = True
+    TEMPLATE_DEBUG = DEBUG
+    LOG_LEVEL = logging.DEBUG
+
 
 ADMINS = (
-    ('Pedro Kroger', 'pedro.kroger@gmail.com'),
+    ('Pedro Kroger', 'kroger@pedrokroger.net'),
 )
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'data.db',
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
 TIME_ZONE = 'America/Bahia'
 LANGUAGE_CODE = 'en-us'
-
 SITE_ID = 1
 USE_I18N = True
 USE_L10N = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-#STATIC_ROOT = ''
-
-# URL prefix for admin static files -- CSS, JavaScript and images.
-# Make sure to use a trailing slash.
-# Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
-
-# Additional locations of static files
-#STATICFILES_DIRS = (
-#    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-#    # Always use forward slashes, even on Windows.
-#    # Don't forget to use absolute paths, not relative paths.
-#)
-
-# List of finder classes that know how to find static files in
-# various locations.
-#STATICFILES_FINDERS = (
-#    'django.contrib.staticfiles.finders.FileSystemFinder',
-#    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-##    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-#)
-
-# Make this unique, and don't share it with anybody.
 SECRET_KEY = '2#t0gkj0w_1nn&jq!(a68q*50d#t41!btf6s44#+v7$czw-187'
 
-# List of callables that know how to import templates from various sources.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -88,8 +99,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'visimus.urls'
 
 TEMPLATE_DIRS = (
-    os.path.join(SITE_ROOT, 'templates'),
-    #'/Users/kroger/Dropbox/Code/visimus/visimus/templates/',
+    os.path.join(MAINDIR, 'visimus', 'templates'),
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
@@ -104,36 +114,27 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.admindocs',
+    'django.contrib.staticfiles',
     'visimus.matrix',
-    'visimus.arrow'
-)
+    'south',
+    'django_nose',
+    )
 
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+NOSE_ARGS = [
+    '--color',
+    '--with-coverage',
+    '--cover-package=testapp',
+    '--cover-html',
+    '--nologcapture',
+]
+
+AUTH_PROFILE_MODULE = "testapp.UserProfile"
+
+logging.basicConfig(filename=LOGFILE,level=LOG_LEVEL,
+                    format='%(asctime)s %(levelname)s %(message)s')
+logger = logging.getLogger(__name__)
